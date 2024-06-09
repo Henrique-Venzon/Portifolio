ec434e8ce87ba013ead6a9c63817bca53919651b
Henrique Francisco Venzon <143184156+Henrique-Venzon@users.noreply.github.com>
2024-06-09 20:36:06 -0300

    muita coisa

---
+++ js/hiperslinks.js
@@ -0,0 +1,6 @@
+document.getElementById('Inicio').addEventListener('click', function() {
+    window.location.href = 'index.html'; 
+});
+document.getElementById('projetos').addEventListener('click', function() {
+    window.location.href = 'projetos.html'; 
+});
