ea4d5c280a3f48a556732c225823882d4d02ede9
Henrique Francisco Venzon <143184156+Henrique-Venzon@users.noreply.github.com>
2024-06-30 16:13:09 -0300

    mudei tudo

---
+++ js/reveal.js
@@ -0,0 +1,58 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.front-animation',
+    {
+    duration: 1800,
+    distance: '100px',
+    origin: 'top'
+})
+revelar.reveal('.front-p-animation',
+    {
+    duration: 1800,
+    delay:200,
+    distance: '100px',
+    origin: 'bottom'
+})
+revelar.reveal('.eu-animation',
+    {
+    delay: 100,
+    duration: 1800,
+    distance: '80px',
+    origin: 'right'
+})
+revelar.reveal('.textWhite-animation',
+    {
+    delay:200,
+    duration: 2100,
+    distance: '100px',
+    origin: 'left'
+})
+revelar.reveal('.textBlue-animation',
+    {
+    delay:200,
+    duration: 2100,
+    distance: '100px',
+    origin: 'right'
+})
+revelar.reveal('.p1-animation',
+    {
+    delay:500,
+    duration: 2100,
+    distance: '100px',
+    origin: 'right'
+})
+revelar.reveal('.p2-animation',
+    {
+    delay:600,
+    duration: 1500,
+    distance: '70px',
+    origin: 'left'
+})
+revelar.reveal('.p3-animation',
+    {
+    delay:700,
+    duration: 1500,
+    distance: '70px',
+    origin: 'right'
+})
