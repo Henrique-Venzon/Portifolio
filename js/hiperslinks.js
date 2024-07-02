ebd1766ba4e118ea20711602f246a63a891b9729
Henrique Francisco Venzon <143184156+Henrique-Venzon@users.noreply.github.com>
2024-07-02 12:21:58 -0300

    atualização

--- js/hiperslinks.js
+++ js/hiperslinks.js
@@ -1,3 +1,6 @@
 document.getElementById('link-page').addEventListener('click', function() {
     window.location.href = 'https://link-bio-alpha.vercel.app/'; 
 });
+document.getElementById('code-monet').addEventListener('click', function() {
+    window.location.href = 'https://codemonet-iago-souzas-projects.vercel.app/'; 
+});
