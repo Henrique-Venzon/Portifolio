c277365b9150c1884926dd7d9e4463bd0d92fb2b
Henrique Francisco Venzon <143184156+Henrique-Venzon@users.noreply.github.com>
2024-10-24 12:09:50 -0300

    quase finalizado

---
+++ js/rede-social.js
@@ -0,0 +1,15 @@
+document.getElementById('linkedlin').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/henrique-venzon/', '_blank');
+});
+document.getElementById('whatsapp').addEventListener('click', function() {
+    window.open('https://wa.me/5547997334286', '_blank');
+});
+document.getElementById('github').addEventListener('click', function() {
+    window.open('https://github.com/Henrique-Venzon', '_blank');
+});
+document.getElementById('instragam').addEventListener('click', function() {
+    window.open('https://www.instagram.com/henriquevenzon_/', '_blank');
+});
+document.getElementById('email').addEventListener('click', function() {
+    window.open('https://mail.google.com/mail/?view=cm&fs=1&to=venzonhenriquef@gmail.com', '_blank');
+});
